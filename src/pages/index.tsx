801259e5caa4744bbe4c6c985c3b4ce919b0d5e7
Nebula <infinitewormhole@gmail.com>
2022-04-06 15:14:06 -0400

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className='container flex items-center p-4 mx-auto min-h-screen justify-center'>
+			<main>
+				<h1 className='font-mono text-xl code'>Cosmic - Launching soon</h1>
+			</main>
+		</div>
+	);
+}
